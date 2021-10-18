d1f2d8fd452e4da89ba22c865db82461fca6f7b7
Colm Flanagan <colm@hiroco.io>
2021-10-18 18:48:09 +1100

    New qr code models and instructions

---
+++ models/model.sh
@@ -0,0 +1,5 @@
+cp -r apple ~/.gazebo/models
+cp -r banana ~/.gazebo/models
+cp -r car ~/.gazebo/models
+cp -r orange ~/.gazebo/models
+cp waffle_pi.model ~/turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/turtlebot3_houses
